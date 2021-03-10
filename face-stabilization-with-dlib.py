bf3066bc8c25d1fe3bd92a7597007b2df1582cf9
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-03-10 15:25:09 +0300

    fix

---
+++ face-stabilization-with-dlib.py
@@ -0,0 +1,61 @@
+
+import cv2
+import dlib
+import numpy as np
+
+def shapePoints(shape, dtype="int"):
+	coords = np.zeros((68, 2), dtype=dtype)
+	for i in range(0, 68):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+	return coords
+
+def rectPoints(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return (x, y, w, h)
+
+def stabilization(cx, cy, frame):
+	x = 725
+	y = 485
+	return x-cx, y-cy
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+back = frame.copy()
+back = cv2.resize(back, (1450,970))
+back = cv2.GaussianBlur(back, (99,99), 0)
+back_copy = back.copy()
+back_copy = cv2.GaussianBlur(back_copy, (99,99), 0)
+
+while True:
+	ret, frame = cap.read()
+	copy_frame = frame.copy()
+	if not ret:
+		break
+	rects = detector(frame, 0)
+	for rect in rects:
+		shape = predictor(frame, rect)
+		points = shapePoints(shape)
+		(x, y, w, h) = rectPoints(rect)
+		resized = frame[y: y+h, x:x+w]
+		cy = int((2*y+h)/2)
+		cx = int((2*x+w)/2)
+		color = (255,255,255)
+		cv2.circle(frame, (cx, cy), 4, color, 2)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+		x,y = stabilization(cx,cy,frame)
+
+	back[y:y+480, x:x+640] = frame[0:480, 0:640]
+	cv2.rectangle(back, (405, 245), (405 + 640, 245 + 480), color, 2)
+	cv2.imshow('back', back)
+	back[y:y+480, x:x+640] = back_copy[y:y+480, x:x+640]
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
